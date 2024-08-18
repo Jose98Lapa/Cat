f573733787b301419c9d46a6aaaee6a300277915
José Lapa <josemmlapa@gmail.com>
2024-08-18 21:56:40 +0100

    Added repository to listviewmodel

--- CatMate/ViewModels/ListViewModel.swift
+++ CatMate/ViewModels/ListViewModel.swift
@@ -10,20 +10,30 @@ import Foundation
 protocol ListViewModelProtocol {
     
     var breeds: [Breed] { get }
+    var repository: BreedRepository { get }
     func fetchBreeds()
-    func favouriteBreed()
+    func favouriteBreed(byId id: UUID)
 }
 
 class ListViewModel: ObservableObject, ListViewModelProtocol {
 
-    
     @Published private(set) var breeds: [Breed] = []
+    private var repository: BreedRepository
+    private var page: Int = 0
+    private var itemsPerPage: Int = 20
+    
+    init(repository: BreedRepository) {
+        self.repository = repository
+    }
     
     func fetchBreeds() {
         let breed = Breed()
         breeds.append(breed)
     }
     
-    func favouriteBreed() {
+    func favouriteBreed(byId id: UUID) {
+        if let indexOfBreed = breeds.firstIndex(where: { $0.id == id }) {
+            breeds[indexOfBreed].isFavourite.toggle()
+        }
     }
 }
