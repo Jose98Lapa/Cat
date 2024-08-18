2a07ebf998cb0bed3a30d64ef671e73b8e01f484
José Lapa <josemmlapa@gmail.com>
2024-08-18 21:58:08 +0100

    Created breed model

--- CatMate/Models/Breed.swift
+++ CatMate/Models/Breed.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
-struct Breed {
-    
+struct Breed: Identifiable {
+    let id: UUID? = UUID(uuidString: "5dd5f046-721e-4769-b0d7-e99c32f84041")
+    var isFavourite: Bool = false
 }
